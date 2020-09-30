ad5cc9408a740dfe5b08193bcce122ab079f81f2
zile91 <ivozilic@hotmail.com>
2020-09-30 16:11:02 +0200

    Edit Contacts 2

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,7 +5,7 @@ import { AiFillLinkedin } from 'react-icons/ai';
 import { AiFillGithub } from 'react-icons/ai';
 import { AiOutlineInstagram } from 'react-icons/ai';
 import office from '../../image/office.jpg';
-import { CV_Zilic_Ivo } from '../../CV/CV_Zilic_Ivo.pdf';
+import CV_Zilic_Ivo from '../../CV/CV_Zilic_Ivo.pdf';
 
 
 export default function Profile() {
@@ -17,7 +17,7 @@ export default function Profile() {
                 <h1>My name is Ivo</h1>
                 <h3>Front-End Developer</h3>
                 <p>I can call myself a Junior Front-End Developer. I like to do that, and I would like to do that to the end. I learn quickly, adopt new technologies, and I'm always ready to improve my knowledge...</p>
-                <button><a href={CV_Zilic_Ivo}>Download CV</a></button>
+                <button><a href={CV_Zilic_Ivo} target="_black">Download CV</a></button>
             </div>
             <div className='profile-center'>
                 <p>FULL NAME: Ivo Žilić</p>
